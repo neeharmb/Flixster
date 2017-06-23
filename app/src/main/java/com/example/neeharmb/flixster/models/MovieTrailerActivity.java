a62eed166c0e889827da1185c37cdf3da28b5e7c
Neehar Banerjee <neeharmb@fb.com>
2017-06-23 14:11:52 -0700

    added Youtube Feature

---
+++ app/src/main/java/com/example/neeharmb/flixster/models/MovieTrailerActivity.java
@@ -0,0 +1,15 @@
+package com.example.neeharmb.flixster.models;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.neeharmb.flixster.R;
+
+public class MovieTrailerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_trailer);
+    }
+}
